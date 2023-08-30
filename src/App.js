b871bd3a87df848997be82ab5a955d942663ca89
premc1208 <115663455+premc1208@users.noreply.github.com>
2023-08-30 22:12:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [search, setSearch] = useState('');
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <center>
+        <form>
+          <h2>Gallery snapshots</h2>
+          <input type="text" value={search} />
+          <br />
+          <br />
+          <input type="submit" value="Search" />
+        </form>
+      </center>
     </div>
   );
 }
